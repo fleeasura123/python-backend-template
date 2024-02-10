d687793a4d180588c0b6e0ce1bbab6893d5d8910
fleeasura123 <roberoliverladeras@gmail.com>
2024-02-10 12:29:40 +0800

    Update connection pool size

--- connection.py
+++ connection.py
@@ -6,7 +6,7 @@ config = dotenv_values(".env")
 
 CONNECTION_STRING = f"postgresql+psycopg://{config['DB_USER']}:{config['DB_PASSWORD']}@{config['DB_HOST']}:{config['DB_PORT']}/{config['DB_NAME']}"
 
-engine = create_engine(CONNECTION_STRING)
+engine = create_engine(CONNECTION_STRING, pool_size=80)
 
 Session = sessionmaker(bind=engine)
 

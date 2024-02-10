f27a6ae488fc6b07bdf3ff498d5db37eb177d084
fleeasura123 <roberoliverladeras@gmail.com>
2024-02-10 12:00:11 +0800

    Update connection.py with PostgreSQL connection string

--- connection.py
+++ connection.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import dotenv_values
 
-
 config = dotenv_values(".env")
 
 CONNECTION_STRING = f"postgresql+psycopg://{config['DB_USER']}:{config['DB_PASSWORD']}@{config['DB_HOST']}:{config['DB_PORT']}/{config['DB_NAME']}"
